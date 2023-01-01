b0ea2deb21ae1a79d1502dbd0b2c302086f87a87
vaishalibokadiya <vaishali.bokadiya@gmail.com>
2023-01-01 14:10:40 +0530

    changed baseURL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "https://memories-server-skvn.onrender.com",
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://memories-server-skvn.onrender.com",
+  // baseURL: "http://localhost:5000/",
 });
 
 API.interceptors.request.use((req) => {
